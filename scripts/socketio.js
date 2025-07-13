1ab680243798e9ae229d7c6759d47495ef9fa5af
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-07-13 15:13:12 +0800

    Changed Indentation

--- scripts/socketio.js
+++ scripts/socketio.js
@@ -1,13 +1,13 @@
 const socket = io('https://socketio-f317.onrender.com');
-        
-socket.on('connect', () => {
-    $("#connectionStatus").html('connected');
 
-    socket.emit('connectToConnection', randomId, (response) => {
-        console.log(response.status);
-    });
+socket.on('connect', () => {
+  $("#connectionStatus").html('connected');
+  
+  socket.emit('connectToConnection', randomId, (response) => {
+    console.log(response.status);
+  });
 });
 
 socket.on('disconnect', () => {
-    $("#connectionStatus").html('disconnected');
+  $("#connectionStatus").html('disconnected');
 });
