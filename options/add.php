4ba927c34c7dff20bc80410dc0b19eaa22e6156c
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-07-17 18:41:27 +0000

    Fixed header logo

--- options/add.php
+++ options/add.php
@@ -8,7 +8,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="../vendor/jquery-3.7.1.min.js"></script>
-        <link rel="icon" href="../assets/logo.png" type="image/x-icon">
+        <link rel="icon" href="../assets/images/logo.svg" type="image/x-icon">
         <style type="text/css">
             @font-face {
                 font-family: space-grotesk-regular;
