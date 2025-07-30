815d921bfe15be57e1dcf266d3edd8fc9082cc74
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-07-30 08:51:28 +0800

    Implemented auto reconnect when disconnected feature

--- scripts/socketio.js
+++ scripts/socketio.js
@@ -1,11 +1,23 @@
 const socket = io('https://socketio-f317.onrender.com');
         
 socket.on('connect', () => {
-    $("#connectionStatus").html('connected');
+    $("#connectionStatus").html('connecting');
 
+    function tryConnecting(){
+        try{
+            console.log("Trying to connect");
+            connect();
+        }catch(error){
+            console.log("error", error);
+            setTimeout(tryConnecting(), 1000);
+        }
+    }
+
+    tryConnecting();
+/*
     socket.emit('connectToConnection', randomId, (response) => {
         console.log(response.status);
-    });
+    });*/
 });
 
 socket.on('disconnect', () => {
